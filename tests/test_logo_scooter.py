af6053a5ba9090b149ba3404303b94f10667928f
mrsMistral <obalzak@list.ru>
2026-02-09 20:56:06 +0100

    Добавила тестовый класс

--- tests/test_logo_scooter.py
+++ tests/test_logo_scooter.py
@@ -1,13 +1,12 @@
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from urls import BASE_URL
-from locators import L
 from pages.main_page import MainPage
 
-def test_click_scooter_logo_opens_main_page(driver):
-    page = MainPage(driver)
-    page.open()
+class TestMainPage:
 
-    page.click_scooter_logo()
+    def test_click_scooter_logo_opens_main_page(self, driver):
+        page = MainPage(driver)
+        page.open()
 
-    assert driver.current_url == BASE_URL
+        page.click_scooter_logo()
+
+        assert driver.current_url == BASE_URL
