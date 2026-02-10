f97064c6f7564c8867c486e5dca347ec58a197b3
mrsMistral <obalzak@list.ru>
2026-02-10 18:21:12 +0100

    Добавила класс в test_order.py

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,37 +1,40 @@
 import pytest
 from pages.order_page import OrderPage
 
-# ---------- Тестовые данные ----------
-test_data = [
-    # формат: name, surname, address, metro, phone, date, rent_period, color
-    ("Мария", "Пономарева", "ул. Ленина, 1", "Сокольники", "+79161234567", "01.03.2026", "двое суток", "чёрный жемчуг"),
-    ("Иван", "Иванов", "ул. Пушкина, 10", "Красносельская", "+79261234567", "02.03.2026", "двое суток", "чёрный жемчуг")
-]
 
-# ---------- Параметризация кнопки заказа ----------
-@pytest.mark.parametrize("order_button_position", ["top", "bottom"])
-@pytest.mark.parametrize("name,surname,address,metro,phone,date,rent_period,color", test_data)
-def test_order_flow(driver, order_button_position, name, surname, address, metro, phone, date, rent_period, color):
-    """Позитивный сценарий: заказ через верхнюю или нижнюю кнопку"""
-    page = OrderPage(driver)
-    page.open()
+class TestOrder:
 
-    # ---------- Клик по нужной кнопке ----------
-    if order_button_position == "top":
-        page.click_order_top()
-    else:
-        page.click_order_bottom()
+    @pytest.mark.parametrize("order_button_position", ["top", "bottom"])
+    @pytest.mark.parametrize(
+        "name,surname,address,metro,phone,date,rent_period,color",
+        [
+            ("Мария", "Пономарева", "ул. Ленина, 1", "Сокольники", "+79161234567", "01.03.2026", "двое суток", "чёрный жемчуг"),
+            ("Иван", "Иванов", "ул. Пушкина, 10", "Красносельская", "+79261234567", "02.03.2026", "двое суток", "чёрный жемчуг"),
+        ]
+    )
+    def test_order_flow(
+        self,
+        driver,
+        order_button_position,
+        name,
+        surname,
+        address,
+        metro,
+        phone,
+        date,
+        rent_period,
+        color
+    ):
+        page = OrderPage(driver)
+        page.open()
 
-    # ---------- Шаг 1: данные клиента ----------
-    page.fill_step1(name, surname, address, metro, phone)
+        if order_button_position == "top":
+            page.click_order_top()
+        else:
+            page.click_order_bottom()
 
-    # ---------- Шаг 2: детали заказа + кнопка "Заказать" ----------
-    page.fill_step2(date, rent_period, color)
+        page.fill_step1(name, surname, address, metro, phone)
+        page.fill_step2(date, rent_period, color)
+        page.confirm_order()
 
-    # ---------- Подтверждение заказа на баннере ----------
-    page.confirm_order()
-
-    # ---------- Проверка успешного заказа ----------
-    assert page.is_order_successful(), "Заказ не был оформлен"
-
-    
+        assert page.is_order_successful(), "Заказ не был оформлен"
